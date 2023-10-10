e1c2086edb37ef48a4c3944a35e6ddbbcd0285fd
LukaszCiaston-coder <lukasz.ciaston@interia.pl>
2023-10-10 16:41:46 +0200

    genre search

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,16 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import '../Movies/Movies.css';
 import { useDarkMode } from '../Utils/DarkMode';
 
-// Użyj URL obrazka "Not Found" z zewnętrznego źródła
 const notFoundImageUrl =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCMq4cGfAmaJAYVpXFPLY57EzVip1FTMK-ETQH1aU24VD-bYx5wJ4srHFP99zAgqXBvfQ&usqp=CAU';
 
 const Movies = () => {
   const [searchKeyword, setSearchKeyword] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [genres, setGenres] = useState([]);
   const { darkMode } = useDarkMode();
+
+  useEffect(() => {
+    // Pobierz listę dostępnych gatunków z API
+    const fetchGenres = async () => {
+      try {
+        const apiKey = '9a72da818298f390a1dbda79726b9d32';
+        const response = await fetch(
+          `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`
+        );
+        const data = await response.json();
+        setGenres(data.genres);
+      } catch (error) {
+        console.error('Error while fetching genres:', error);
+      }
+    };
+
+    fetchGenres();
+  }, []);
 
   const handleSearchInputChange = event => {
     setSearchKeyword(event.target.value);
@@ -49,6 +67,31 @@ const Movies = () => {
     return title;
   };
 
+  const handleGenreClick = async genreId => {
+    try {
+      const apiKey = '9a72da818298f390a1dbda79726b9d32';
+      const response = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&with_genres=${genreId}`
+      );
+      const data = await response.json();
+
+      const resultsWithImages =
+        data.results.length > 0
+          ? data.results
+          : [
+              {
+                poster_path: null,
+                title: 'Not Found',
+                id: 'not-found',
+              },
+            ];
+
+      setSearchResults(resultsWithImages);
+    } catch (error) {
+      console.error('Error while fetching data:', error);
+    }
+  };
+
   return (
     <div className={`movies-container ${darkMode ? 'dark-mode' : ''}`}>
       <h2>Search Your Movie</h2>
@@ -59,6 +102,20 @@ const Movies = () => {
         onChange={handleSearchInputChange}
       />
       <button onClick={handleSearch}>Search</button>
+
+      {/* Przyciski gatunków */}
+      <div className="genre-buttons">
+        {genres.map(genre => (
+          <button
+            key={genre.id}
+            onClick={() => handleGenreClick(genre.id)}
+            className="genre-button"
+          >
+            {genre.name}
+          </button>
+        ))}
+      </div>
+
       <ul className="movies-list">
         {searchResults.map(movie => (
           <li key={movie.id}>
@@ -71,7 +128,7 @@ const Movies = () => {
                 />
               ) : (
                 <img
-                  src={notFoundImageUrl} // Obrazek "Not Found" z zewnętrznego URL
+                  src={notFoundImageUrl}
                   alt="Not Found"
                   className="movie-thumbnail"
                 />
